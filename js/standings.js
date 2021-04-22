7732977518c5c509ef00d86b990115225526082f
Przemysław Chyra <pchyra98@gmail.com>
2021-04-22 21:56:16 +0200

    Added teams standings

--- js/standings.js
+++ js/standings.js
@@ -1,8 +1,13 @@
 
 import {drivers} from "./drivers.js";
+import {teams} from "./teams.js";
 import {races} from "./races.js";
 
-
+/*
+-=-=-=-=-=-=-=-=-=-=-
+STANDINGS FOR DRIVERS
+-=-=-=-=-=-=-=-=-=-=-
+*/
 let driversMap = new Map();
 driversMap.set("hamilton", drivers.hamilton.getPoints());
 driversMap.set("bottas", drivers.bottas.getPoints());
@@ -36,12 +41,44 @@ function sorty(a, b) {
  }
 //sort Arr
 pointsArr.sort(sorty);
-
-function showStandings(){
+//Show drivers standings
+function showDriversStandings(){
     for(let i = 0; i < pointsArr.length; i++){
         console.log(i+1 + ". " + pointsArr[i][0] + " " + pointsArr[i][1])
     }
 }
-showStandings();
+showDriversStandings();
 
+/*
+-=-=-=-=-=-=-=-=-=-
+STANDINGS FOR TEAMS
+-=-=-=-=-=-=-=-=-=-
+*/
+let teamsMap = new Map();
+teamsMap.set("mercedes", teams.mercedes.getTeamPoints());
+teamsMap.set("redbull", teams.redbull.getTeamPoints());
+teamsMap.set("mclaren", teams.mclaren.getTeamPoints());
+teamsMap.set("ferrari", teams.ferrari.getTeamPoints());
+teamsMap.set("alphatauri", teams.alphatauri.getTeamPoints());
+teamsMap.set("astonmartin", teams.astonmartin.getTeamPoints());
+teamsMap.set("alpine", teams.alpine.getTeamPoints());
+teamsMap.set("alfaromeo", teams.alfaromeo.getTeamPoints());
+teamsMap.set("williams", teams.williams.getTeamPoints());
+teamsMap.set("haas", teams.haas.getTeamPoints());
+
+let teamsPointsArr = [];
+//Change Map to Arr for sort method
+for(let value of teamsMap){
+    teamsPointsArr.push(value);    
+}
+
+//sort Arr
+teamsPointsArr.sort(sorty);
+//Show Teams Standings
+function showTeamsStandings(){
+    for(let i = 0; i < teamsPointsArr.length; i++){
+        console.log(i+1 + ". " + teamsPointsArr[i][0] + " " + teamsPointsArr[i][1])
+    }
+}
+showTeamsStandings();
 export {driversMap}; 
